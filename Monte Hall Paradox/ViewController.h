b8af82540e454c5bd74f538cc8bbf16aa77a5e65
Chloe Marshall <chloe.r.marshall@gmail.com>
2017-02-10 10:43:32 -0600

    Working application that demonstrates the Monte Hall Paradox. Some minor future changes may be made, such as design of interface.

--- Monte Hall Paradox/ViewController.h
+++ Monte Hall Paradox/ViewController.h
@@ -7,8 +7,43 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *door1;
+@property (weak, nonatomic) IBOutlet UIImageView *door2;
+@property (weak, nonatomic) IBOutlet UIImageView *door3;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *doorSelector;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *keepSwitchSelector;
+@property (weak, nonatomic) IBOutlet UILabel *selectLabel;
+@property (weak, nonatomic) IBOutlet UIButton *revealButton;
+@property (weak, nonatomic) IBOutlet UIButton *showButton;
+@property (weak, nonatomic) IBOutlet UIButton *resetButton;
+@property (weak, nonatomic) IBOutlet UIImageView *arrow1;
+@property (weak, nonatomic) IBOutlet UIImageView *arrow2;
+@property (weak, nonatomic) IBOutlet UIImageView *arrow3;
+@property (weak, nonatomic) IBOutlet UILabel *keepTotalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *keepWinLabel;
+@property (weak, nonatomic) IBOutlet UILabel *keepLossLabel;
+@property (weak, nonatomic) IBOutlet UILabel *switchTotalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *switchWinLabel;
+@property (weak, nonatomic) IBOutlet UILabel *switchLossLabel;
+
+- (IBAction)doorSelected:(id)sender;
+- (IBAction)keepSwitch:(id)sender;
+- (IBAction)revealDoorButtonAction:(id)sender;
+- (IBAction)showResultButtonAction:(id)sender;
+- (IBAction)resetButtonAction:(id)sender;
+- (void) showArrow;
+- (void) revealDoor1;
+- (void) revealDoor2;
+- (void) revealDoor3;
+- (void) resetDoors;
+
+
+
+
 
 
 @end
--- Monte Hall Paradox/ViewController.m
+++ Monte Hall Paradox/ViewController.m
@@ -9,14 +9,75 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    AVAudioPlayer *_audioPlayerWin;
+    AVAudioPlayer *_audioPlayerLose;
+}
 
 @end
 
 @implementation ViewController
 
+@synthesize door1;
+@synthesize door2;
+@synthesize door3;
+@synthesize doorSelector;
+@synthesize keepSwitchSelector;
+@synthesize selectLabel;
+@synthesize revealButton;
+@synthesize showButton;
+@synthesize resetButton;
+@synthesize arrow1;
+@synthesize arrow2;
+@synthesize arrow3;
+@synthesize keepTotalLabel;
+@synthesize keepWinLabel;
+@synthesize keepLossLabel;
+@synthesize switchWinLabel;
+@synthesize switchLossLabel;
+@synthesize switchTotalLabel;
+
+int doorWinner;
+int playerChoice;
+int doorRevealed;
+int keepTotalCounter;
+int switchTotalCounter;
+int keepWinCounter;
+int switchWinCounter;
+int keepLossCounter;
+int switchLossCounter;
+bool isWinner;
+
+
+UIImageView  *imageView1;
+UIImageView  *imageView2;
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSString *winnerPath = [NSString stringWithFormat:@"%@/WINNER.WAV", [[NSBundle mainBundle] resourcePath]];
+    NSString *loserPath = [NSString stringWithFormat:@"%@/LoserHorns.wav", [[NSBundle mainBundle] resourcePath]];
+    NSURL *soundUrlWin = [NSURL fileURLWithPath:winnerPath];
+    NSURL *soundUrlLose = [NSURL fileURLWithPath:loserPath];
+    
+    _audioPlayerWin = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrlWin error:nil];
+    _audioPlayerLose = [[AVAudioPlayer alloc] initWithContentsOfURL:soundUrlLose error:nil];
+
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
+    doorWinner = ((arc4random() % 3) + 1);
+    keepTotalCounter = 0;
+    switchTotalCounter = 0;
+    keepWinCounter = 0;
+    switchWinCounter = 0;
+    keepLossCounter = 0;
+    switchLossCounter = 0;
+    
+    
+    
 }
 
 
@@ -26,4 +87,258 @@
 }
 
 
+- (IBAction)doorSelected:(id)sender {
+    selectLabel.hidden = TRUE;
+    revealButton.hidden = FALSE;
+    doorSelector.hidden = TRUE;
+    
+    if(doorSelector.selectedSegmentIndex == 0){
+        playerChoice = 1;
+    }
+    if(doorSelector.selectedSegmentIndex == 1){
+        playerChoice = 2;
+    }
+    if(doorSelector.selectedSegmentIndex == 2){
+        playerChoice = 3;
+    }
+    //Why would we need to create a new function (show arrow) if we can just put the methods in here
+    [self showArrow];
+}
+
+- (void) showArrow {
+    if(playerChoice == 1){
+        arrow1.hidden = FALSE;
+        arrow2.hidden = TRUE;
+        arrow3.hidden = TRUE;
+    }
+    if(playerChoice == 2){
+        arrow1.hidden = TRUE;
+        arrow2.hidden = FALSE;
+        arrow3.hidden = TRUE;
+    }
+    if(playerChoice == 3){
+        arrow1.hidden = TRUE;
+        arrow2.hidden = TRUE;
+        arrow3.hidden = FALSE;
+    }
+    
+}
+
+- (IBAction)keepSwitch:(id)sender {
+    if(keepSwitchSelector.selectedSegmentIndex == 1){
+        if(doorRevealed == 1){
+            if(playerChoice == 2)
+            {
+                playerChoice = 3;
+            }else{
+                playerChoice = 2;
+            }
+        }
+        if(doorRevealed == 2){
+            if(playerChoice == 1)
+            {
+                playerChoice = 3;
+            }else{
+                playerChoice = 1;
+            }
+        }
+        if(doorRevealed == 3){
+            if(playerChoice == 1)
+            {
+                playerChoice = 2;
+            }else{
+                playerChoice = 1;
+            }
+        }
+        [self showArrow];
+        switchTotalCounter += 1;
+        if(playerChoice == doorWinner){
+            switchWinCounter += 1;
+            isWinner = TRUE;
+        }else{
+            switchLossCounter += 1;
+            isWinner = FALSE;
+        }
+    }else{
+        keepTotalCounter += 1;
+        if(playerChoice == doorWinner){
+            keepWinCounter += 1;
+            isWinner = TRUE;
+        }else{
+            keepLossCounter += 1;
+            isWinner = FALSE;
+        }
+    }
+    keepSwitchSelector.hidden = TRUE;
+    showButton.hidden = FALSE;
+}
+
+- (IBAction)revealDoorButtonAction:(id)sender {
+    bool selectionRepeat = TRUE;
+    
+    while(selectionRepeat){
+        doorRevealed = ((arc4random() % 3) +1);
+        if(doorRevealed != doorWinner && doorRevealed != playerChoice)
+        {
+            selectionRepeat = FALSE;
+        }
+
+    }
+    if(doorRevealed == 1) [self revealDoor1];
+    if(doorRevealed == 2) [self revealDoor2];
+    if(doorRevealed == 3) [self revealDoor3];
+    
+    revealButton.hidden = TRUE;
+    keepSwitchSelector.hidden = FALSE;
+}
+
+- (void) revealDoor1{
+    
+    if(doorWinner == 1){
+        imageView2 = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"winner"]];
+    } else {
+        imageView2 = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"looser"]];
+    }
+    [imageView2 setFrame:CGRectMake(36, 34, 82, 72)];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:(1.50)];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(transitionDidStop: finished: context:)];
+    [UIView setAnimationTransition: UIViewAnimationTransitionFlipFromLeft forView: door1 cache:YES];
+    
+    [door1 addSubview:imageView2];
+    
+    [UIView commitAnimations];
+    
+}
+- (void) revealDoor2{
+    
+    if(doorWinner == 2){
+        imageView2 = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"winner"]];
+    } else {
+        imageView2 = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"looser"]];
+    }
+    [imageView2 setFrame:CGRectMake(36, 34, 82, 72)];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:(1.50)];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(transitionDidStop: finished: context:)];
+    [UIView setAnimationTransition: UIViewAnimationTransitionFlipFromLeft forView: door2 cache:YES];
+    
+    [door2 addSubview:imageView2];
+    
+    [UIView commitAnimations];
+    
+}
+- (void) revealDoor3{
+    
+    if(doorWinner == 3){
+        imageView2 = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"winner"]];
+    } else {
+        imageView2 = [[UIImageView alloc] initWithImage: [UIImage imageNamed:@"looser"]];
+    }
+    [imageView2 setFrame:CGRectMake(36, 34, 82, 72)];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:(1.50)];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(transitionDidStop: finished: context:)];
+    [UIView setAnimationTransition: UIViewAnimationTransitionFlipFromLeft forView: door3 cache:YES];
+    
+    [door3 addSubview: imageView2];
+    
+    [UIView commitAnimations];
+    
+}
+
+- (void) transitionDidStop: (NSString *)animationID finished:(BOOL) finished context: (void*) context{
+    NSLog(@"Transition Finished");
+}
+
+- (IBAction)showResultButtonAction:(id)sender {
+    [self revealDoor1];
+    [self revealDoor2];
+    [self revealDoor3];
+    
+    showButton.hidden = TRUE;
+    resetButton.hidden = FALSE;
+    
+    keepTotalLabel.text = [[NSString alloc] initWithFormat:@"%d", keepTotalCounter];
+    keepWinLabel.text = [[NSString alloc] initWithFormat:@"%d", keepWinCounter];
+    keepLossLabel.text = [[NSString alloc] initWithFormat:@"%d", keepLossCounter];
+    switchTotalLabel.text = [[NSString alloc] initWithFormat:@"%d", switchTotalCounter];
+    switchWinLabel.text = [[NSString alloc] initWithFormat:@"%d", switchWinCounter];
+    switchLossLabel.text = [[NSString alloc] initWithFormat:@"%d", switchLossCounter];
+    
+    if(isWinner){
+        [_audioPlayerWin play];
+    }else{
+        [_audioPlayerLose play];
+    }
+
+
+}
+
+- (IBAction)resetButtonAction:(id)sender {
+    
+    doorWinner = ((arc4random() % 3) +1);
+    resetButton.hidden = TRUE;
+    
+    [self resetDoors];
+    
+    arrow1.hidden = TRUE;
+    arrow2.hidden = TRUE;
+    arrow3.hidden = TRUE;
+    doorSelector.hidden = FALSE;
+    
+    [doorSelector setSelectedSegmentIndex:UISegmentedControlNoSegment];
+    [keepSwitchSelector setSelectedSegmentIndex:UISegmentedControlNoSegment];
+    
+}
+
+- (void) resetDoors {
+    
+    imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"door"]];
+    [imageView1 setFrame:CGRectMake(0, 0, 163, 156)];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1.50];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(transitionDidStop: finished: context:)];
+    [UIView setAnimationTransition: UIViewAnimationTransitionFlipFromRight forView: door1 cache:YES];
+    [imageView2 removeFromSuperview];
+    [door1 addSubview:imageView1];
+    [UIView commitAnimations];
+    
+    
+    imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"door"]];
+    [imageView1 setFrame:CGRectMake(0, 0, 163, 156)];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1.50];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(transitionDidStop: finished: context:)];
+    [UIView setAnimationTransition: UIViewAnimationTransitionFlipFromRight forView: door2 cache:YES];
+    [imageView2 removeFromSuperview];
+    [door2 addSubview:imageView1];
+    [UIView commitAnimations];
+    
+    
+    imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"door"]];
+    [imageView1 setFrame:CGRectMake(0, 0, 163, 156)];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1.50];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(transitionDidStop: finished: context:)];
+    [UIView setAnimationTransition: UIViewAnimationTransitionFlipFromRight forView: door3 cache:YES];
+    [imageView2 removeFromSuperview];
+    [door3 addSubview:imageView1];
+    [UIView commitAnimations];
+    
+    
+    
+}
 @end
